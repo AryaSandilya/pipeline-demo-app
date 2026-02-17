643c9a870225db586d5c2b131e6ecf061126b8d6
Sojourner <aryasandilya41@gmail.com>
2026-02-17 08:57:19 +0530

    Update App.java

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -6,5 +6,7 @@ public class App {
         System.out.println("Build Date: " + java.time.LocalDateTime.now());
         System.out.println("Java Version: " + System.getProperty("java.version"));
         System.out.println("Hi there!");
+        System.out.println("Version 2.0 - Auto deployed by Jenkins!"); 
     }
 }
+
